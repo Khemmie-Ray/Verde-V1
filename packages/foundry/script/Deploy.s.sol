db38bb691f713c79a0d6571a74950c2eef0ea8f2
joshdfg <slimmy0310@gmail.com>
2024-08-30 07:33:33 -0400

    scaffold init

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -4,34 +4,43 @@ pragma solidity ^0.8.19;
 import "../contracts/YourContract.sol";
 import "./DeployHelpers.s.sol";
 
-contract DeployScript is ScaffoldETHDeploy {
-  error InvalidPrivateKey(string);
+contract DeployScript is Script {
+    error InvalidPrivateKey(string);
 
-  function run() external {
-    uint256 deployerPrivateKey = setupLocalhostEnv();
-    if (deployerPrivateKey == 0) {
-      revert InvalidPrivateKey(
-        "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
-      );
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        YourContract yourContract = new YourContract(
+            vm.addr(deployerPrivateKey)
+        );
+        console.logString(
+            string.concat(
+                "YourContract deployed at: ",
+                vm.toString(address(yourContract))
+            )
+        );
+
+        vm.stopBroadcast();
+
+        writeAddressesToFile(address(yourContract), "contract Address");
     }
-    vm.startBroadcast(deployerPrivateKey);
 
-    YourContract yourContract = new YourContract(vm.addr(deployerPrivateKey));
-    console.logString(
-      string.concat(
-        "YourContract deployed at: ", vm.toString(address(yourContract))
-      )
-    );
+    function test() public {}
 
-    vm.stopBroadcast();
+    function writeAddressesToFile(address addr, string memory text) public {
+        string memory filename = "./deployed_contracts.txt";
 
-    /**
-     * This function generates the file containing the contracts Abi definitions.
-     * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
-     * This function should be called last.
-     */
-    exportDeployments();
-  }
-
-  function test() public { }
+        vm.writeLine(
+            filename,
+            "-------------------------------------------------"
+        );
+        vm.writeLine(filename, text);
+        vm.writeLine(filename, vm.toString(addr));
+        vm.writeLine(
+            filename,
+            "-------------------------------------------------"
+        );
+    }
 }
